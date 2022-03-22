70f39f6a0e0f0f66b29f5e8b88c0c11cadbb8c34
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-22 12:05:09 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_LINQ_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_LINQ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             List<Prisioner> prisioners = new List<Prisioner> { new Prisioner("Гусь Хрустальный", "Антиправительственное"), new Prisioner("Вомбат Картофаныч", "Ложные показания"),
+            new Prisioner("Ирина Алегровна", "Антиправительственное"), new Prisioner("Алексей Индюшкин", "Убийство")};
+
+            foreach (var prisioner in prisioners)
+            {
+                Console.WriteLine(prisioner.Name);
+            }
+
+            var prisionersInPrison = prisioners.RemoveAll(prisioner => prisioner.Crime.Contains("Антиправительственное"));
+
+            Console.SetCursorPosition(0, 5);
+
+            foreach (var prisioner in prisioners)
+            {
+                Console.WriteLine(prisioner.Name);
+            }
+        }
+    }
+
+    class Prisioner
+    {
+        public string Name { get; private set; }
+
+        public string Crime { get; private set; }
+
+        public Prisioner(string name, string crime)
+        {
+            Name = name;
+            Crime = crime;
+        }
+    }
+}
